13e7047d6adb754359009daba0d3f7bdcf7491fe
ahmednabil14 <ahmednabil14499@gmail.com>
2024-10-01 00:56:50 +0300

    connecting patient and doctor profile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,9 +15,9 @@ const Navbar = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const storedUser = localStorage.getItem('user');
+    const storedUser = JSON.parse(localStorage.getItem('user'));
     if (storedUser) {
-      setUser(JSON.parse(storedUser));
+      setUser(storedUser);
     }
   }, []);
   const handelLogout = (e) => {
