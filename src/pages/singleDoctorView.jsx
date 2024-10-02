9af81b1d5cf21aab855f45fa08b13028d83a6513
Omar-elasrigy <merooscar87th@gmail.com>
2024-10-02 18:08:42 +0300

    each comment and review

--- src/pages/singleDoctorView.jsx
+++ src/pages/singleDoctorView.jsx
@@ -6,31 +6,39 @@ import Footer from "../components/footer/footer"
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import Loading from "../components/loading/loading";
+import CommentsAndRatingsList from "../components/singleDoctorView/commentsAndRatings/commentsAndRatings";
+import FetchPatients from "../components/singleDoctorView/fetchPatients/fetchPatients";
 export default function SingleDoctorView() 
+
 { 
     const { id } = useParams();
     const [doctor, setDoctor] = useState();
+    const [patientsData, setPatientsData] = useState([]);
 
     useEffect(() => {
         axios
         .get(`http://127.0.0.1:8000/api/doctor/${id}`) 
         .then((response) => {
-            console.log("Doctor data:", response.data); 
           setDoctor(response.data);
         })
         .catch((error) => {
           console.error("Error fetching the doctors' data", error);
         });
-    }, [doctor,id]);
+    }, [id]);
 
-  console.log("hello" ,doctor)
+    const handleFetchPatients = (data) => {
+        setPatientsData(data);
+    };
+   
     return (
         <> 
         <Navbar/>
         {doctor ? (
             <>
-                <HeaderDocPat key={doctor.id} doctor={doctor} />
-                <Bar key={doctor.id} doctor={doctor} />
+                <HeaderDocPat  doctor={doctor} />
+                <Bar doctor={doctor} />
+                <FetchPatients onFetch={handleFetchPatients} />
+                <CommentsAndRatingsList doctor={doctor} patientsData={patientsData} />
             </>
         ) : (
             <Loading/>
