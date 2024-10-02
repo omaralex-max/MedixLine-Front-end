59e3ad033d203fb855060215f8a9464dd278c40e
mohamedemad <mohamedemad123456789101112@gmail.com>
2024-10-02 19:09:50 +0300

    fixng single doctor view problem

--- src/pages/singleDoctorView.jsx
+++ src/pages/singleDoctorView.jsx
@@ -17,7 +17,7 @@ export default function SingleDoctorView()
 
     useEffect(() => {
         axios
-        .get(http://127.0.0.1:8000/api/doctor/${id}) 
+        .get(`http://127.0.0.1:8000/api/doctor/${id}`) 
         .then((response) => {
             // console.log("Doctor data:", response.data); 
           setDoctor(response.data);
